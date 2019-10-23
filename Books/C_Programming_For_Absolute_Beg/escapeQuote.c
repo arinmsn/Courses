b4dca100d2df598c545907c57a08ca676370ee90
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-10-23 04:46:18 -0700

    \" Escape Quote

---
+++ Books/C_Programming_For_Absolute_Beg/escapeQuote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\"Anfangen ist leicht, beharren eine Kunst.\n");
+    printf("To begin is easy, to persist is art. -German Proverb\"");
+    return 0;
+}
