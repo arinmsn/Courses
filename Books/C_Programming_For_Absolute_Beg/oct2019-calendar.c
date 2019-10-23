16b4dbf8e9389e7358f8ddae3e153c88c39308bd
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-10-23 05:11:21 -0700

    October month - 2019 Calendar using \t & \n

---
+++ Books/C_Programming_For_Absolute_Beg/oct2019-calendar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("Sun\tMon\tTue\tWed\tThu\tFri\tSat\n");
+    printf("\t\t1\t2\t3\t4\t5\n");
+    printf("6\t7\t8\t9\t10\t11\t12\n");
+    printf("13\t14\t15\t16\t17\t18\t19\n");
+    printf("20\t21\t22\t23\t24\t25\t26\n");
+    printf("27\t28\t29\t30\t31\n");
+    return 0;
+}
