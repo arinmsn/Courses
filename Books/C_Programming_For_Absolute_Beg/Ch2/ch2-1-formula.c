c6134b2902a1476051f623eabd07bb521c41cc10
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-10-24 05:23:15 -0700

    Solution to 1st challenge

---
+++ Books/C_Programming_For_Absolute_Beg/Ch2/ch2-1-formula.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+
+{
+  /*Formula to print f = (a - b)(x - y) */
+  int iA = 5, iB = 1;
+  int iX = 10, iY = 5;
+  int iF = 0; 
+
+  printf("\na = 5, b = 1\nx = 10, y = 5\n\nf = (a - b)(x - y) = %i\n", iF = (iA -iB)*(iX - iY));
+  return 0;
+}
