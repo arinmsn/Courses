a8b207174fdb1fddd09131acaced20c32f5450b1
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-11-02 05:26:30 -0700

    Increment by 5, starting from 1-95 (<100)

--- Books/C_Programming_For_Absolute_Beg/Ch4/ch4-1-counterUp.c
+++ Books/C_Programming_For_Absolute_Beg/Ch4/ch4-1-counterUp.c
@@ -1,10 +1,24 @@
+// This will print 1, 6, 11, ..., 86, 91, 96
+//#include <stdio.h>
+
+//int main()
+//{
+//    int iNum;
+//    for (iNum = 1; iNum <= 100; iNum += 5){
+//        printf("\n%i", iNum);
+//    }
+//    return 0;
+//} //end main function
+
 #include <stdio.h>
 
 int main()
 {
-    int iNum;
-    for (iNum = 1; iNum <= 100; iNum += 5){
-        printf("\n%i", iNum);
-    }
+    int iNum = 5;
+    printf("\n1");
+    do{
+        printf("\n%d", iNum);
+        iNum += 5;
+    } while (iNum < 100);
     return 0;
 } //end main function
