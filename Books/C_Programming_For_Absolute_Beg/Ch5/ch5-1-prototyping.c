65d61c7e04600b072eac2e73aca3910a6e89ba18
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-11-09 20:36:17 -0800

    Function prototyping

---
+++ Books/C_Programming_For_Absolute_Beg/Ch5/ch5-1-prototyping.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+float fgetRemainder(int, int);  // function that divides two numbers and returns the remainder
+float fMaxNumber(int, int);     // function that finds the larger of two numbers and returns the result
+void printMenu(void);           // function that prints an ATM menu--it receives no parameters and returns no value
+
+int main()
+{
+    printf("\n");
+}
