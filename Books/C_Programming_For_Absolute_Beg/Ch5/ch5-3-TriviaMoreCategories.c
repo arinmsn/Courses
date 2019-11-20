d861660f7030a5324b52e348631a1748292e0d90
Arin Minasian <45381088+arinmsn@users.noreply.github.com>
2019-11-19 21:25:03 -0800

    Completed Trivia with 2 more categories

--- Books/C_Programming_For_Absolute_Beg/Ch5/ch5-3-TriviaMoreCategories.c
+++ Books/C_Programming_For_Absolute_Beg/Ch5/ch5-3-TriviaMoreCategories.c
@@ -64,3 +64,110 @@ main() {
                 } //end of switch
         } while (giResponse != 5);
 } // end of main()
+
+/*****************************************************
+FUNCTION DEFINITION
+*****************************************************/
+
+int sportsQuestion(void) {
+
+        int iAnswer = 0;
+
+        system("clear");
+        printf("\tSports Question\n");
+        printf("\nWhat University did NFL star Deon Sanders attend? ");
+        printf("\n\n1\tUniversity of Miami\n");
+        printf("2\tCalifornia State University\n");
+        printf("3\tIndiana University\n");
+        printf("4\tFlorida State University\n");
+        printf("\nEnter your selection: ");
+        scanf("%d", &iAnswer);
+
+        return iAnswer;
+
+}
+
+/*****************************************************
+FUNCTION DEFINITION
+*****************************************************/
+
+int geographyQuestion(void) {
+
+        int iAnswer = 0;
+
+        system("clear");
+        printf("\tGeography Question\n");
+        printf("\nWhat is the state capitol of Florida? ");
+        printf("\n\n1\tPenescola\n");
+        printf("2\tTallahassee\n");
+        printf("3\tJacksonville\n");
+        printf("4\tMiami\n");
+        printf("\nEnter your selection: ");
+        scanf("%d", &iAnswer);
+
+        return iAnswer;
+
+} 
+
+/*****************************************************
+FUNCTION DEFINITION
+*****************************************************/
+
+int moviesQuestion(void) {
+
+        int iAnswer = 0;
+
+        system("clear");
+        printf("\tMovies Question\n");
+        printf("\nIn the Marvel cinematic universe, what is the name of Thor's home planet? ");
+        printf("\n\n1\tMorag\n");
+        printf("2\tSakaar\n");
+        printf("3\tAsgard\n");
+        printf("4\tAakon\n");
+        printf("\nEnter your selection: ");
+        scanf("%d", &iAnswer);
+
+        return iAnswer;
+
+}
+
+/*****************************************************
+FUNCTION DEFINITION
+*****************************************************/
+
+int mathQuestion(void) {
+
+        int iAnswer = 0;
+
+        system("clear");
+        printf("\tMath Question\n");
+        printf("\ne^(i*pi) equals what number? ");
+        printf("\n\n1\t\n");
+        printf("2\t0\n");
+        printf("3\te+1\n");
+        printf("4\t-1\n");
+        printf("\nEnter your selection: ");
+        scanf("%d", &iAnswer);
+
+        return iAnswer;
+
+}
+
+/*****************************************************
+FUNCTION DEFINITION
+*****************************************************/
+
+void pause(int inNum) {
+
+        int iCurrentTime = 0;
+        int iElapsedTime = 0;
+
+        iCurrentTime = time(NULL);
+
+        do {
+
+                iElapsedTime = time(NULL);
+
+        } while ((iElapsedTime - iCurrentTime) < inNum);
+
+} 
