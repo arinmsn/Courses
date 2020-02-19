5c06c6cf73a32450e68b781cdd3266506ca3975c
arinmsn <arinmsn@gmail.com>
2020-02-18 17:07:42 -0800

    02_maxSum of Subarrays & 3 passing tests

---
+++ Grokking/Sliding_Window/02_maxSum_ofSubarray.js
@@ -0,0 +1,14 @@
+function maxSumSubArray(k, arr) {
+  let [maxSum, windowSum, windwoStart] = [0, 0, 0];
+  for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    if (windowEnd >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[windwoStart];
+      windwoStart++;
+    }
+  }
+  return maxSum;
+}
+
+exports.maxSumSubArray = maxSumSubArray;
