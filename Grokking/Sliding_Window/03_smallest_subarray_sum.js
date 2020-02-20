26a6720bf00de1668822fa9d9da28c431eae3d7d
arinmsn <arinmsn@gmail.com>
2020-02-19 18:27:34 -0800

    Sliding Windows - 03_Smallest subarray given a sum

--- Grokking/Sliding_Window/03_smallest_subarray_sum.js
+++ Grokking/Sliding_Window/03_smallest_subarray_sum.js
@@ -4,9 +4,28 @@ find the length of the smallest contiguous subarray whose sum
 is greater than or equal to ‘S’. Return 0, if no such subarray exists.
 */
 
+/*  Time Complexity: outer for loop runs for all elements
+    and inner while loop processes each element only once. 
+    O(N + N) = O(N)
+    Space Complexity: O(1)
+*/
+
 const smallest_subarray_with_given_sum = function(s, arr) {
-  // TODO: Write your code here
-  return -1;
+  let [windowSum, minLength, windowStart] = [0, Infinity, 0];
+  for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd]; // add next element
+    while (windowSum >= s) {
+      minLength = Math.min(minLength, windowEnd - windowStart + 1);
+      windowSum -= arr[windowStart];
+      windowStart += 1;
+    }
+  }
+
+  if (minLength === Infinity) {
+    return 0;
+  }
+
+  return minLength;
 };
 
 module.exports = { smallest_subarray_with_given_sum };
