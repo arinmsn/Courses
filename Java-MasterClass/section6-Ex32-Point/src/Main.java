c2b682f9b78d831fb369878a661ee9863fe121a1
arinmsn <arinmsn@gmail.com>
2020-04-17 17:54:32 -0700

    Added some code to test to Main file

---
+++ Java-MasterClass/section6-Ex32-Point/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+    }
+}
--- Java-MasterClass/section6-Ex32-Point/src/Point.java
+++ Java-MasterClass/section6-Ex32-Point/src/Point.java
@@ -1,4 +1,4 @@
-public class Point{
+public class Point {
     private int x;
     private int y;
 
