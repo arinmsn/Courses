27f19ec66a004022a8494e924a235a3254a70d1a
arinmsn <arinmsn@gmail.com>
2020-06-13 12:41:20 -0700

    Reading User Input example

---
+++ Java-MasterClass/ReadingUserInput/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your year of birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if (hasNextInt){
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (Enter key)
+
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2020 - yearOfBirth;
+
+            if (age >= 0 && age <= 100){
+                System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.a");
+        }
+        scanner.close();
+    }
+}
