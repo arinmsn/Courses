29b8448843dc75223f253588b372120544ef9550
arinmsn <arinmsn@gmail.com>
2020-06-12 20:35:53 -0700

    Speed Converter exercise

---
+++ Java-MasterClass/SpeedConverter/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+        SpeedConverter.printConversion(10.5);
+    }
+}
