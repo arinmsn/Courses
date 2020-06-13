9a9962dec4029110bc3bd1c6c7f0684266881f7b
arinmsn <arinmsn@gmail.com>
2020-06-13 11:26:53 -0700

    Perfect Number exercise

---
+++ Java-MasterClass/PerfectNumber/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(PerfectNumber.isPerfectNumber(6)); // true
+        System.out.println(PerfectNumber.isPerfectNumber(-2)); // false
+        System.out.println(PerfectNumber.isPerfectNumber(28)); // true
+        System.out.println(PerfectNumber.isPerfectNumber(496)); // true
+        System.out.println(PerfectNumber.isPerfectNumber(8128)); // true
+    }
+}
