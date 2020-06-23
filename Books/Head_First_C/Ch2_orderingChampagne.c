8fed1d051b23ed6f0794ea89ddc982f6e2398a63
arinmsn <arinmsn@gmail.com>
2020-06-23 11:09:07 -0700

    We can simply our code and use scanf to get integer entered by user.

--- Books/Head_First_C/Ch2_orderingChampagne.c
+++ Books/Head_First_C/Ch2_orderingChampagne.c
@@ -4,16 +4,11 @@
 int main()
 {
     int stock = 180;
-    // We are able to enter up to 2 digits.
-    char order_string[4];
     int order;
     while (stock > 0)
     {
         printf("%i glasses left\n", stock);
-        scanf("%3s", order_string);
-        // Converting string into a number
-        order = atoi(order_string);
-        stock = stock - order;
+        scanf("%i", &order);
         printf("You ordered %i glasses\n", order);
     }
     puts("We're out of stock!");
